1997c6b1088596f2b8747ccce5a0f05f5293939b
heymichaelw <heymichaelw@gmail.com>
2017-01-17 10:23:20 -0500

    started object oriented programming

--- Day2/Program.cs
+++ Day2/Program.cs
@@ -6,10 +6,48 @@ using System.Threading.Tasks;
 
 namespace Day2
 {
+    //class definition
+    class Dog
+    {
+        int Age;
+        public string name;
+
+        public Dog(string _name)
+        {
+            Age = 0;
+            name = _name;
+        }
+
+        public void HappyBirthday()
+        {
+            Age++;
+        }
+
+        public int GetAge()
+        {
+            return Age;
+        }
+
+        public int GetAgeDogYears()
+        {
+            return Age * 7;
+        }
+    }
+
+
     class Program
     {
         static void Main(string[] args)
         {
+            var fred = new Dog("Fred");
+            fred.HappyBirthday();
+            fred.HappyBirthday();
+            fred.HappyBirthday();
+            var age = fred.GetAge();
+            var agedogyears = fred.GetAgeDogYears();
+            Console.WriteLine(age);
+            Console.WriteLine(agedogyears);
+            Console.WriteLine(fred.name);
         }
     }
 }
