6a086ebff3a837aaeee89ccee500ad58751f7287
heymichaelw <heymichaelw@gmail.com>
2017-01-18 10:45:30 -0500

    added intro to subclassing

---
+++ Day3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var square = new Square(4, 4);
+            double squareArea = square.Area();
+            Console.WriteLine(squareArea);
+
+            Shape circle = new Circle(10);
+            double circleArea = circle.Area();
+            Console.WriteLine(circleArea);
+
+            var nonogram = new Shape();
+            double nonArea = nonogram.Area();
+            Console.WriteLine(nonArea);
+        }
+    }
+}
---
+++ Day3/Shape.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day3
+{
+    class Shape
+    {
+        public double height;
+        public double width;
+        public double radius;
+        private string shapetype;
+
+        public Shape()
+        {
+
+        }
+
+        //circle constructor
+        public Shape(double _radius)
+        {
+            radius = _radius;
+        }
+
+        //square constructor
+        public Shape(double _height, double _width)
+        {
+            height = _height;
+            width = _width;
+        }
+
+        public virtual double Area()
+        {
+            return Double.PositiveInfinity;
+        }
+    }
+}
